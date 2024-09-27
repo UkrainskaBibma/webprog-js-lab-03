03023d109fa1d7e8f941b25aafdcc278a033ad2b
UkrainskaBibma <sasha.kumanets.06@gmail.com>
2024-09-27 15:32:50 +0300

    Kumanets

--- js/task-1.js
+++ js/task-1.js
@@ -15,3 +15,4 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
   console.log(makeTransaction(10, 5000, 8000)); 
   console.log(makeTransaction(8, 2000, 10000));
   console.log(makeTransaction(10, 500, 5000)); 
+  
